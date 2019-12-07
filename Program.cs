e21a6012b1fb37a02f7759430688d788f80b74d4
Adriano AE <adae@outlook.pt>
2019-12-07 14:43:05 +0000

    • Code cleanup

--- Program.cs
+++ Program.cs
@@ -1,14 +1,79 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace EFCoreComputedColumnProblem
 {
+    public class TaxesPercentage
+    {
+        public int Value { get; set; }
+        public decimal Multiplier { get; set; }
+
+        public TaxesPercentage(int value)
+        {
+            Value = value;
+        }
+    }
+
     public class Taxes
     {
         public int Id { get; set; }
         public TaxesPercentage Percentage { get; set; }
+        public bool IsDeleted { get; set; }
     }
+
+    //█████████████████████████████████████████████████████████████████████████████████████████████
+
+    class IvaEntityConfiguration : IEntityTypeConfiguration<Taxes>
+    {
+        public void Configure(EntityTypeBuilder<Taxes> builder)
+        {
+            builder.OwnsOne(t => t.Percentage, b =>
+            {
+                b.Property(v => v.Value)
+                    .HasColumnName("Percentage")
+                    .IsRequired();
+
+                b.Property(p => p.Multiplier)
+                    .HasColumnType("decimal(3,2)")
+                    .HasColumnName(nameof(TaxesPercentage.Multiplier))
+                    .HasComputedColumnSql("(CONVERT([decimal](3,2),CONVERT([decimal](3,0),[Percentage])/(100.00)+(1))) PERSISTED");
+            });
+        }
+    }
+
+    //█████████████████████████████████████████████████████████████████████████████████████████████
+
+    public class BloggingContext : DbContext
+    {
+        private readonly ILoggerFactory Logger
+            = LoggerFactory.Create(c => c.AddConsole());
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+            => modelBuilder.ApplyConfigurationsFromAssembly(typeof(BloggingContext).Assembly);
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                .UseLoggerFactory(Logger)
+                .EnableSensitiveDataLogging()
+                .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Test;ConnectRetryCount=0");
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(e => e.Properties.Any(p => p.Metadata.Name == "IsDeleted") && e.State == EntityState.Deleted))
+            {
+                entry.State = EntityState.Modified;
+                entry.CurrentValues["IsDeleted"] = true;
+            }
+
+            return base.SaveChanges();
+        }
+    }
+
+    //█████████████████████████████████████████████████████████████████████████████████████████████
 
     public class Program
     {
@@ -26,6 +91,8 @@ namespace EFCoreComputedColumnProblem
 
                 context.SaveChanges();
             }
+
+            Console.Clear();
 
             using (var context = new BloggingContext())
             {
